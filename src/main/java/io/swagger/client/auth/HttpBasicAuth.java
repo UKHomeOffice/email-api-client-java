27f0dba21841786fdae25178c61d43d1dfea42e2
Billie Alice Thompson <billie@purplebooth.co.uk>
2015-10-19 16:33:43 +0100

    Tidied some classes

--- src/main/java/io/swagger/client/auth/HttpBasicAuth.java
+++ src/main/java/io/swagger/client/auth/HttpBasicAuth.java
@@ -2,40 +2,39 @@ package io.swagger.client.auth;
 
 import io.swagger.client.Pair;
 
-import java.util.Map;
-import java.util.List;
-
-import java.io.UnsupportedEncodingException;
 import javax.xml.bind.DatatypeConverter;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Map;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2015-10-19T15:11:18.643+01:00")
 public class HttpBasicAuth implements Authentication {
-  private String username;
-  private String password;
+    private String username;
+    private String password;
 
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  @Override
-  public void applyToParams(List<Pair> queryParams, Map<String, String> headerParams) {
-    String str = (username == null ? "" : username) + ":" + (password == null ? "" : password);
-    try {
-      headerParams.put("Authorization", "Basic " + DatatypeConverter.printBase64Binary(str.getBytes("UTF-8")));
-    } catch (UnsupportedEncodingException e) {
-      throw new RuntimeException(e);
+    public String getUsername() {
+        return username;
     }
-  }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public void applyToParams(List<Pair> queryParams, Map<String, String> headerParams) {
+        String str = (username == null ? "" : username) + ":" + (password == null ? "" : password);
+        try {
+            headerParams.put("Authorization", "Basic " + DatatypeConverter.printBase64Binary(str.getBytes("UTF-8")));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/io/swagger/client/model/TemplatedEmail.java
+++ src/main/java/io/swagger/client/model/TemplatedEmail.java
@@ -1,13 +1,14 @@
 package io.swagger.client.model;
 
-import io.swagger.client.StringUtil;
-import java.util.*;
-import java.util.Map;
-
-
-
-import io.swagger.annotations.*;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.client.StringUtil;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -15,107 +16,112 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  **/
 @ApiModel(description = "Enough information to populate and send and email based on a template")
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2015-10-19T15:11:18.643+01:00")
-public class TemplatedEmail   {
-  
-  private String sender = null;
-  private String subject = null;
-  private String htmlTemplate = null;
-  private String textTemplate = null;
-  private Map<String, Object> variables = new HashMap<String, Object>();
-  private List<String> recipients = new ArrayList<String>();
+public class TemplatedEmail {
 
-  
-  /**
-   * Sender of the email in format \"example@example.org\" or \"Annie Example <annie@example.com>\"
-   **/
-  @ApiModelProperty(required = true, value = "Sender of the email in format \"example@example.org\" or \"Annie Example <annie@example.com>\"")
-  @JsonProperty("sender")
-  public String getSender() {
-    return sender;
-  }
-  public void setSender(String sender) {
-    this.sender = sender;
-  }
+    private String sender = null;
+    private String subject = null;
+    private String htmlTemplate = null;
+    private String textTemplate = null;
+    private Map<String, Object> variables = new HashMap<String, Object>();
+    private List<String> recipients = new ArrayList<String>();
 
-  
-  /**
-   * Subject of the email
-   **/
-  @ApiModelProperty(required = true, value = "Subject of the email")
-  @JsonProperty("subject")
-  public String getSubject() {
-    return subject;
-  }
-  public void setSubject(String subject) {
-    this.subject = subject;
-  }
 
-  
-  /**
-   * If a recipient uses HTML to view their they will see this template
-   **/
-  @ApiModelProperty(required = true, value = "If a recipient uses HTML to view their they will see this template")
-  @JsonProperty("htmlTemplate")
-  public String getHtmlTemplate() {
-    return htmlTemplate;
-  }
-  public void setHtmlTemplate(String htmlTemplate) {
-    this.htmlTemplate = htmlTemplate;
-  }
+    /**
+     * Sender of the email in format \"example@example.org\" or \"Annie Example <annie@example.com>\"
+     **/
+    @ApiModelProperty(required = true, value = "Sender of the email in format \"example@example.org\" or \"Annie Example <annie@example.com>\"")
+    @JsonProperty("sender")
+    public String getSender() {
+        return sender;
+    }
 
-  
-  /**
-   * If a recipient uses TXT to view their they will see this template
-   **/
-  @ApiModelProperty(required = true, value = "If a recipient uses TXT to view their they will see this template")
-  @JsonProperty("textTemplate")
-  public String getTextTemplate() {
-    return textTemplate;
-  }
-  public void setTextTemplate(String textTemplate) {
-    this.textTemplate = textTemplate;
-  }
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
 
-  
-  /**
-   * Variables to pass to both templates with string keys, and any JSON Number, String, Array, Object as the value
-   **/
-  @ApiModelProperty(required = true, value = "Variables to pass to both templates with string keys, and any JSON Number, String, Array, Object as the value")
-  @JsonProperty("variables")
-  public Map<String, Object> getVariables() {
-    return variables;
-  }
-  public void setVariables(Map<String, Object> variables) {
-    this.variables = variables;
-  }
 
-  
-  /**
-   * Array of recipients in an array of strings in format \"example@example.org\" or \"Annie Example <annie@example.com>\"
-   **/
-  @ApiModelProperty(required = true, value = "Array of recipients in an array of strings in format \"example@example.org\" or \"Annie Example <annie@example.com>\"")
-  @JsonProperty("recipients")
-  public List<String> getRecipients() {
-    return recipients;
-  }
-  public void setRecipients(List<String> recipients) {
-    this.recipients = recipients;
-  }
+    /**
+     * Subject of the email
+     **/
+    @ApiModelProperty(required = true, value = "Subject of the email")
+    @JsonProperty("subject")
+    public String getSubject() {
+        return subject;
+    }
 
-  
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 
-  @Override
-  public String toString()  {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class TemplatedEmail {\n");
-    
-    sb.append("    sender: ").append(StringUtil.toIndentedString(sender)).append("\n");
-    sb.append("    subject: ").append(StringUtil.toIndentedString(subject)).append("\n");
-    sb.append("    htmlTemplate: ").append(StringUtil.toIndentedString(htmlTemplate)).append("\n");
-    sb.append("    textTemplate: ").append(StringUtil.toIndentedString(textTemplate)).append("\n");
-    sb.append("    variables: ").append(StringUtil.toIndentedString(variables)).append("\n");
-    sb.append("    recipients: ").append(StringUtil.toIndentedString(recipients)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
+
+    /**
+     * If a recipient uses HTML to view their they will see this template
+     **/
+    @ApiModelProperty(required = true, value = "If a recipient uses HTML to view their they will see this template")
+    @JsonProperty("htmlTemplate")
+    public String getHtmlTemplate() {
+        return htmlTemplate;
+    }
+
+    public void setHtmlTemplate(String htmlTemplate) {
+        this.htmlTemplate = htmlTemplate;
+    }
+
+
+    /**
+     * If a recipient uses TXT to view their they will see this template
+     **/
+    @ApiModelProperty(required = true, value = "If a recipient uses TXT to view their they will see this template")
+    @JsonProperty("textTemplate")
+    public String getTextTemplate() {
+        return textTemplate;
+    }
+
+    public void setTextTemplate(String textTemplate) {
+        this.textTemplate = textTemplate;
+    }
+
+
+    /**
+     * Variables to pass to both templates with string keys, and any JSON Number, String, Array, Object as the value
+     **/
+    @ApiModelProperty(required = true, value = "Variables to pass to both templates with string keys, and any JSON Number, String, Array, Object as the value")
+    @JsonProperty("variables")
+    public Map<String, Object> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(Map<String, Object> variables) {
+        this.variables = variables;
+    }
+
+
+    /**
+     * Array of recipients in an array of strings in format \"example@example.org\" or \"Annie Example <annie@example.com>\"
+     **/
+    @ApiModelProperty(required = true, value = "Array of recipients in an array of strings in format \"example@example.org\" or \"Annie Example <annie@example.com>\"")
+    @JsonProperty("recipients")
+    public List<String> getRecipients() {
+        return recipients;
+    }
+
+    public void setRecipients(List<String> recipients) {
+        this.recipients = recipients;
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class TemplatedEmail {\n");
+
+        sb.append("    sender: ").append(StringUtil.toIndentedString(sender)).append("\n");
+        sb.append("    subject: ").append(StringUtil.toIndentedString(subject)).append("\n");
+        sb.append("    htmlTemplate: ").append(StringUtil.toIndentedString(htmlTemplate)).append("\n");
+        sb.append("    textTemplate: ").append(StringUtil.toIndentedString(textTemplate)).append("\n");
+        sb.append("    variables: ").append(StringUtil.toIndentedString(variables)).append("\n");
+        sb.append("    recipients: ").append(StringUtil.toIndentedString(recipients)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
 }
